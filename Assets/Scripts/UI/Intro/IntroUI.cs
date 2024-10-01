f0b32d768d2b8b337b4af73f456a268d528e5a55
kimgyeran <kgh08813@gmail.com>
2024-10-02 03:58:48 +0900

    Intro Scene Implement
    Add Dotween
    Fix Pixel Resolution
    Implement IntroUI Interaction

---
+++ Assets/Scripts/UI/Intro/IntroUI.cs
@@ -0,0 +1,65 @@
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class IntroUI : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject Bgd;
+    public GameObject Charecter;
+    public GameObject Title;
+    public GameObject Text;
+
+    AsyncOperation ao;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Charecter.GetComponent<RectTransform>().DOAnchorPosX(0f, 1f).SetEase(Ease.InExpo).SetAutoKill();
+
+
+
+        Title.GetComponent<RectTransform>().DOAnchorPosY(-420f, 1f).SetEase(Ease.InExpo).onComplete = () =>
+        {
+
+            Title.GetComponent<RectTransform>().DOAnchorPosY(-440f, 1f).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo).SetAutoKill();
+            StartCoroutine(blink());
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ao != null)
+        {
+            Debug.Log(ao.progress);
+            if (ao.progress >= 0.9f)
+            {
+                DOTween.KillAll(); StopCoroutine(blink());
+                ao.allowSceneActivation = true;
+            }
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (ao != null)
+            return;
+        Debug.Log("Enter");
+        ao = SceneManager.LoadSceneAsync(1);
+        ao.allowSceneActivation = false;
+    }
+
+
+
+    IEnumerator blink()
+    {
+        while (true)
+        {
+            Text.SetActive(!Text.activeSelf);
+            yield return new WaitForSeconds(Text.activeSelf ? 1f : 0.5f);
+        }
+    }
+}
